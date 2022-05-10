74f7d64958796f43fcf2f88a16c4a3be80aba310
ArinMnw <66235730+ArinMnw@users.noreply.github.com>
2022-05-10 16:43:00 +0700

    Add project files.

---
+++ HangfireDemo/Controllers/HangfireController.cs
@@ -0,0 +1,42 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class HangfireController : Controller
+    {
+        private readonly HttpClient httpClient;
+        private readonly IBackgroundJobClient backgroundJobClient;
+        public HangfireController(IBackgroundJobClient backgroundJobClient, IHttpClientFactory httpClientFactory)
+        {
+            this.backgroundJobClient=backgroundJobClient;
+            this.httpClient=httpClientFactory.CreateClient("ApiService");
+        }
+        [HttpGet("{datetime}")]
+        public void ScheduleHangfire(DateTime dateTime)
+        {
+            DateTimeOffset dateTimeOffset = DateTime.SpecifyKind(dateTime, DateTimeKind.Local);
+            var jobId = backgroundJobClient.Schedule(() => TestHangfire2(dateTime), dateTimeOffset);
+            Console.WriteLine("Hangfire Schedule Begin!!");
+            //RecurringJob.AddOrUpdate(() => TestHangfire2(), "*/15 * * * *");
+            //RecurringJob.AddOrUpdate("myrecurringjob", () => Console.WriteLine("Recurring!"), Cron.Daily);
+        }
+
+        [HttpGet("{datetime}")]
+        public async Task TestHangfire2(DateTime dateTime)
+        {
+            await httpClient.GetAsync("https://localhost:7084/Testapi");
+            Console.WriteLine($"Hangfire Schedule Done!! {dateTime.ToString("dd MM yyyy HH:mm")}");
+        }
+
+        [HttpGet("{datetime}/{jobId}")]
+        public void EnqueueHangfire(DateTime dateTime, string jobId)
+        {
+            backgroundJobClient.Enqueue(() => TestHangfire2(dateTime));
+            backgroundJobClient.Delete(jobId);
+            Console.WriteLine("Hangfire Enqueue Job Now!!");
+        }
+
+    }
+}
---
+++ HangfireDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using HangfireDemo.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHangfire(configuration =>
+{
+    configuration.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"));
+});
+builder.Services.AddHangfireServer();
+builder.Services.AddHttpClient("ApiService");
+//IBackgroundJobClient client = new BackgroundJobClient(new SqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+//client.Enqueue(() =>
+//    Console.WriteLine("Hello world from Hangfire")
+//);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseHangfireDashboard();
+
+app.Run();
